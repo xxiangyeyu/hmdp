0bd56f202862e9987f0a2afe4ac6ea16f1ff1bf6
xxiangyeyu <zimuchao@126.com>
2023-03-02 22:39:02 +0800

    1、解决状态登录刷新的问题

--- src/main/java/pers/xxiangyeyu/config/MvcConfig.java
+++ src/main/java/pers/xxiangyeyu/config/MvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pers.xxiangyeyu.utils.LoginInterceptor;
+import pers.xxiangyeyu.utils.RefreshTokenInterceptor;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -15,7 +16,8 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor(stringRedisTemplate))
+        // 登录拦截器
+        registry.addInterceptor(new LoginInterceptor())
                 .excludePathPatterns(
                       "/shop/**",
                       "/voucher/**",
@@ -24,6 +26,8 @@ public class MvcConfig implements WebMvcConfigurer {
                       "/blog/hot",
                       "/user/code",
                       "/user/login"
-                );
+                ).order(1);
+        // token刷新的拦截器
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
     }
 }
