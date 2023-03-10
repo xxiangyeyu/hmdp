9d86874df7e7b41a85e3da8d5fb5ffa318ae1614
xxiangyeyu <zimuchao@126.com>
2023-03-10 21:48:39 +0800

    1、Redis实现全局唯一id（测试类）

---
+++ src/test/java/pers/xxiangyeyu/HmDianPingApplicationTests.java
@@ -0,0 +1,40 @@
+package pers.xxiangyeyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pers.xxiangyeyu.utils.RedisIdWorker;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+public class HmDianPingApplicationTests {
+
+    @Autowired
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    public void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
+
+}
